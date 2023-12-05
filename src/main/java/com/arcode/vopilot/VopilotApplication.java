70cf9f21c3cad4d145fb3af621ca67edca270dbb
Wilver Arana R <u202023992@upc.edu.pe>
2023-12-05 18:34:45 -0500

    Initial Commit

---
+++ src/main/java/com/arcode/vopilot/VopilotApplication.java
@@ -0,0 +1,13 @@
+package com.arcode.vopilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VopilotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VopilotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arcode/vopilot/VopilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcode.vopilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VopilotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
