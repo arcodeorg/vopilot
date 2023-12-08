1b251f89ec99d5ad681ec68b42d809027cf03b74
Jack Arana <arack.dev@gmail.com>
2023-12-08 12:01:00 -0500

    chore: add url swagger and enable jpa auditing

--- src/main/java/com/arcode/vopilot/VopilotApplication.java
+++ src/main/java/com/arcode/vopilot/VopilotApplication.java
@@ -2,12 +2,17 @@ package com.arcode.vopilot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class VopilotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VopilotApplication.class, args);
+
+        String urlSwagger = "http://localhost:8080/swagger-ui.html";
+        System.out.println("Swagger URL: " + urlSwagger);
     }
 
 }
